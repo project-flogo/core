e5c00edac3f5930ea0dfdb8edec2824d3deed25b
Andrew Snodgrass <git@pointlander.info>
2018-10-18 15:58:45 -0600

    Use first settings element

--- api/api.go
+++ api/api.go
@@ -170,7 +170,7 @@ func (h *Handler) NewAction(handlerAction interface{}, settings ...interface{}) 
 		act = &Action{act: NewProxyAction(v)}
 	case action.Action:
 		if len(settings) > 0 {
-			act, err = newAction(v, settings)
+			act, err = newAction(v, settings[0])
 		} else {
 			act, err = newAction(v, nil)
 		}
