0bb8f6cc825257f1c0d877c48cfdfa479ac40f20
Akshay Gadikar <akshayug@gmail.com>
2018-11-06 11:21:07 -0800

    Trace - 2

--- swagger/swagger.go
+++ swagger/swagger.go
@@ -9,14 +9,18 @@ import (
 
 // Generate generates a Swagger 2.0 document based off of the provided endpoints.
 func Generate(host string, name string, description string, version string, endpoints []Endpoint) ([]byte, error) {
+	fmt.Println("Inside Generate")
 	paths := map[string]interface{}{}
 
 	for _, endpoint := range endpoints {
 		path := map[string]interface{}{}
+		fmt.Println("Before scrubbedpath")
 		parameters, scrubbedPath := swaggerParametersExtractor(endpoint.Path, endpoint.BeginDelim, endpoint.EndDelim)
+		fmt.Println("After swag returns")
 		ok := map[string]interface{}{
 			"description": endpoint.Description,
 		}
+		fmt.Println("path map")
 		path[strings.ToLower(endpoint.Method)] = map[string]interface{}{
 			"description": endpoint.Description,
 			"tags":        []interface{}{endpoint.Name},
@@ -41,17 +45,21 @@ func Generate(host string, name string, description string, version string, endp
 		"host":  host,
 		"paths": paths,
 	}
+	fmt.Println("marshalllll")
 	docs, err := json.MarshalIndent(&swagger, "", "    ")
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println("before return generate")
 	return docs, err
 }
 
 func swaggerParametersExtractor(path string, beginDelim rune, endDelim rune) ([]interface{}, string) {
+	fmt.Println("Inside Swaggerparams")
 	parameters := []interface{}{}
 	routePath := []rune(path)
 	for i := 0; i < len(routePath); i++ {
+		fmt.Println("Inside for")
 		if routePath[i] == beginDelim {
 			key := bytes.Buffer{}
 			for i++; i < len(routePath) && routePath[i] != endDelim; i++ {
@@ -71,5 +79,6 @@ func swaggerParametersExtractor(path string, beginDelim rune, endDelim rune) ([]
 			parameters = append(parameters, parameter)
 		}
 	}
+	fmt.Println("Before swag returns")
 	return parameters, path
 }
