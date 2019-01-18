261af9b16a12ed29753d80d960c1f5412057ae17
Mathieu Debove <mathieu.debove@sqits.net>
2019-01-18 15:35:55 +0100

    Add omitempty to fields not present in default flogo.json

--- action/config.go
+++ action/config.go
@@ -5,13 +5,13 @@ import "encoding/json"
 // Config is the configuration for the Action
 type Config struct {
 	//inline action
-	Ref      string                 `json:"ref"`
-	Type     string                 `json:"type"` //an alias to the ref, can be used if imported
-	Settings map[string]interface{} `json:"settings"`
+	Ref      string                 `json:"ref,omitempty"`
+	Type     string                 `json:"type,omitempty"` //an alias to the ref, can be used if imported
+	Settings map[string]interface{} `json:"settings,omitempty"`
 
 	//referenced action
-	Id string `json:"id"`
+	Id string `json:"id,omitempty"`
 
 	//DEPRECATED
-	Data json.RawMessage `json:"data"`
+	Data json.RawMessage `json:"data,omitempty"`
 }
--- app/config.go
+++ app/config.go
@@ -13,11 +13,12 @@ type Config struct {
 	Type        string `json:"type"`
 	Version     string `json:"version"`
 	Description string `json:"description"`
+	AppModel    string `json:"appModel"`
 
-	Imports    []string           `json:"imports"`
-	Properties []*data.Attribute  `json:"properties"`
-	Channels   []string           `json:"channels"`
-	Triggers   []*trigger.Config  `json:"triggers"`
-	Resources  []*resource.Config `json:"resources"`
-	Actions    []*action.Config   `json:"actions"`
+	Imports    []string           `json:"imports,omitempty"`
+	Properties []*data.Attribute  `json:"properties,omitempty"`
+	Channels   []string           `json:"channels,omitempty"`
+	Triggers   []*trigger.Config  `json:"triggers,omitempty"`
+	Resources  []*resource.Config `json:"resources,omitempty"`
+	Actions    []*action.Config   `json:"actions,omitempty"`
 }
