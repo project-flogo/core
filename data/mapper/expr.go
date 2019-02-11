79182e6490abdbdc277381d813ceb633fd0070a9
Tracy Li <tibcotracy@gmail.com>
2019-02-11 10:49:07 -0600

    support array mapping

--- data/mapper/expr.go
+++ data/mapper/expr.go
@@ -7,12 +7,14 @@ import (
 )
 
 type ExprMapperFactory struct {
-	exprFactory expression.Factory
+	exprFactory  expression.Factory
+	arrayFactory ArrayMapperFactory
 }
 
 func NewFactory(resolver resolve.CompositeResolver) Factory {
 	exprFactory := expression.NewFactory(resolver)
-	return &ExprMapperFactory{exprFactory: exprFactory}
+	arrayFactory := ArrayMapperFactory{exprFactory}
+	return &ExprMapperFactory{exprFactory: exprFactory, arrayFactory: arrayFactory}
 }
 
 func (mf *ExprMapperFactory) NewMapper(mappings map[string]interface{}) (Mapper, error) {
@@ -21,19 +23,28 @@ func (mf *ExprMapperFactory) NewMapper(mappings map[string]interface{}) (Mapper,
 		return nil, nil
 	}
 
-	exprMappings := make(map[string]expression.Expr, len(mappings))
-
+	exprMappings := make(map[string]expression.Expr)
 	for key, value := range mappings {
-
-		if strVal, ok := value.(string); ok && len(strVal) > 0 && strVal[0] == '=' {
-			expr, err := mf.exprFactory.NewExpr(strVal[1:])
-			if err != nil {
-				return nil, err
+		if value != nil {
+			if strVal, ok := value.(string); ok && len(strVal) > 0 && strVal[0] == '=' {
+				expr, err := mf.exprFactory.NewExpr(strVal[1:])
+				if err != nil {
+					return nil, err
+				}
+				exprMappings[key] = expr
+			} else if IsArrayMapping(value) {
+				arrayExpr, err := mf.arrayFactory.NewArrayExpr(value)
+				if err != nil {
+					return nil, err
+				}
+				exprMappings[key] = arrayExpr
+			} else {
+				exprMappings[key] = expression.NewLiteralExpr(value)
 			}
-			exprMappings[key] = expr
-		} else {
-			exprMappings[key] = expression.NewLiteralExpr(value)
 		}
+	}
+	if len(exprMappings) <= 0 {
+		return nil, nil
 	}
 
 	return &ExprMapper{mappings: exprMappings}, nil
@@ -44,7 +55,6 @@ type ExprMapper struct {
 }
 
 func (m *ExprMapper) Apply(inputScope data.Scope) (map[string]interface{}, error) {
-
 	output := make(map[string]interface{}, len(m.mappings))
 	for key, expr := range m.mappings {
 		val, err := expr.Eval(inputScope)
