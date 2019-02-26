fa6d8da152bbfcb769cb82dcc5f86481f5a3c90d
Tracy Li <tibcotracy@gmail.com>
2019-02-26 11:53:02 -0600

    Optimized code and add more test cases

--- data/mapper/deprecated_test.go
+++ data/mapper/deprecated_test.go
@@ -127,7 +127,7 @@ func TestPathToObject(t *testing.T) {
 	path := []string{"data", "field", "value"}
 	obj := make(map[string]interface{})
 
-	toObjectFromPath(path, "1234", obj)
+	constructObjectFromPath(path, "1234", obj)
 	v, _ := json.Marshal(obj)
 	fmt.Println(string(v))
 	assert.Equal(t, "1234", obj["data"].(map[string]interface{})["field"].(map[string]interface{})["value"])
@@ -137,7 +137,7 @@ func TestPathToObjectArray(t *testing.T) {
 	path := []string{"data[2]", "field[0]", "value"}
 	obj := make(map[string]interface{})
 
-	toObjectFromPath(path, "1234", obj)
+	constructObjectFromPath(path, "1234", obj)
 	v, _ := json.Marshal(obj)
 	fmt.Println(string(v))
 	assert.Equal(t, "1234", obj["data"].([]interface{})[2].(map[string]interface{})["field"].([]interface{})[0].(map[string]interface{})["value"])
@@ -150,9 +150,9 @@ func TestMultiplePathToObject(t *testing.T) {
 
 	obj := make(map[string]interface{})
 
-	toObjectFromPath(path, "1234", obj)
+	constructObjectFromPath(path, "1234", obj)
 
-	toObjectFromPath(path2, "1234", obj)
+	constructObjectFromPath(path2, "1234", obj)
 
 	v, _ := json.Marshal(obj)
 	fmt.Println(string(v))
@@ -164,13 +164,15 @@ func TestMultiplePathToObjectArray(t *testing.T) {
 	obj := make(map[string]interface{})
 	path2 := []string{"data[4]", "field[0]", "value"}
 
-	toObjectFromPath(path, "1234", obj)
+	constructObjectFromPath(path, "1234", obj)
 
-	toObjectFromPath(path2, "1234", obj)
+	constructObjectFromPath(path2, "1234", obj)
 
 	v, _ := json.Marshal(obj)
 	fmt.Println(string(v))
 	assert.Equal(t, "1234", obj["data"].([]interface{})[2].(map[string]interface{})["field"].([]interface{})[0].(map[string]interface{})["value"])
+	assert.Equal(t, "1234", obj["data"].([]interface{})[4].(map[string]interface{})["field"].([]interface{})[0].(map[string]interface{})["value"])
+
 }
 
 func TestMultiplePathToObjectArray2(t *testing.T) {
@@ -178,11 +180,256 @@ func TestMultiplePathToObjectArray2(t *testing.T) {
 	obj := make(map[string]interface{})
 	path2 := []string{"data", "field", "value[4]"}
 
-	toObjectFromPath(path, 22, obj)
+	constructObjectFromPath(path, 22, obj)
 
-	toObjectFromPath(path2, 33, obj)
+	constructObjectFromPath(path2, 33, obj)
 
 	v, _ := json.Marshal(obj)
 	fmt.Println(string(v))
-	assert.Equal(t, "1234", obj["data"].([]interface{})[2].(map[string]interface{})["field"].([]interface{})[0].(map[string]interface{})["value"])
+	assert.Equal(t, 22, obj["data"].(map[string]interface{})["field"].(map[string]interface{})["value"].([]interface{})[0])
+	assert.Equal(t, 33, obj["data"].(map[string]interface{})["field"].(map[string]interface{})["value"].([]interface{})[4])
+
+}
+
+func TestConvertMappingValue(t *testing.T) {
+	mappings := `{
+        "input": [
+         {
+          "mapTo": "input1",
+          "type": "expression",
+          "value": "$.body.id"
+         },
+         {
+          "mapTo": "input2",
+          "type": "expression",
+          "value": "$.body.name"
+         }
+        ],
+        "output": [
+         {
+          "mapTo": "code",
+          "type": "expression",
+          "value": 200
+         },
+         {
+          "mapTo": "data.return",
+          "type": "expression",
+          "value": "$.res"
+         }
+        ]
+       }`
+
+	mapping := &LegacyMappings{}
+
+	err := json.Unmarshal([]byte(mappings), mapping)
+	assert.Nil(t, err)
+
+	input, output, err := ConvertLegacyMappings(mapping)
+	assert.Nil(t, err)
+
+	v, _ := json.Marshal(input)
+	fmt.Println("input:", string(v))
+
+	v2, _ := json.Marshal(output)
+	fmt.Println("output:", string(v2))
+	assert.Equal(t, "=$.body.id", input["input1"])
+	assert.Equal(t, "=$.body.name", input["input2"])
+
+}
+
+func TestConvertMappingValue2(t *testing.T) {
+	mappings := `{
+        "input": [
+         {
+          "mapTo": "input1.a.b",
+          "type": "expression",
+          "value": "$.body.id"
+         },
+         {
+          "mapTo": "input1.a.c",
+          "type": "expression",
+          "value": "$.body.name"
+         }
+        ],
+        "output": [
+         {
+          "mapTo": "code",
+          "type": "expression",
+          "value": 200
+         },
+         {
+          "mapTo": "data.return",
+          "type": "expression",
+          "value": "$.res"
+         }
+        ]
+       }`
+
+	mapping := &LegacyMappings{}
+
+	err := json.Unmarshal([]byte(mappings), mapping)
+	assert.Nil(t, err)
+
+	input, output, err := ConvertLegacyMappings(mapping)
+	assert.Nil(t, err)
+
+	v, _ := json.Marshal(input)
+	fmt.Println("input:", string(v))
+
+	v2, _ := json.Marshal(output)
+	fmt.Println("output:", string(v2))
+
+	assert.Equal(t, "=$.body.id", input["input1"].(map[string]interface{})["a"].(map[string]interface{})["b"])
+	assert.Equal(t, "=$.body.name", input["input1"].(map[string]interface{})["a"].(map[string]interface{})["c"])
+
+}
+
+func TestConvertMappingValue3(t *testing.T) {
+	mappings := `{
+        "input": [
+         {
+          "mapTo": "input1[0].a.b",
+          "type": "expression",
+          "value": "$.body.id"
+         },
+         {
+          "mapTo": "data[0]",
+          "type": "expression",
+          "value": "$.body.name"
+         },
+		{
+          "mapTo": "data[1]",
+          "type": "expression",
+          "value": "$.body.name"
+         }
+        ],
+        "output": [
+         {
+          "mapTo": "code",
+          "type": "expression",
+          "value": 200
+         },
+         {
+          "mapTo": "data.return",
+          "type": "expression",
+          "value": "$.res"
+         }
+        ]
+       }`
+
+	mapping := &LegacyMappings{}
+
+	err := json.Unmarshal([]byte(mappings), mapping)
+	assert.Nil(t, err)
+
+	input, output, err := ConvertLegacyMappings(mapping)
+	assert.Nil(t, err)
+
+	v, _ := json.Marshal(input)
+	fmt.Println("input:", string(v))
+
+	v2, _ := json.Marshal(output)
+	fmt.Println("output:", string(v2))
+	assert.Equal(t, "=$.body.name", input["data"].([]interface{})[0])
+	assert.Equal(t, "=$.body.name", input["data"].([]interface{})[1])
+	assert.Equal(t, "=$.body.id", input["input1"].([]interface{})[0].(map[string]interface{})["a"].(map[string]interface{})["b"])
+
+	//output
+	assert.Equal(t, "=$.res", output["data"].(map[string]interface{})["return"])
+	assert.Equal(t, float64(200), output["code"])
+
+}
+
+func TestConvertMappingValue4(t *testing.T) {
+	mappings := `{
+        "input": [
+         {
+          "mapTo": "data[0]",
+          "type": "expression",
+          "value": "$.body.name"
+         },
+		{
+          "mapTo": "data[1]",
+          "type": "expression",
+          "value": "$.body.ddd"
+         }
+        ]
+       }`
+
+	mapping := &LegacyMappings{}
+
+	err := json.Unmarshal([]byte(mappings), mapping)
+	assert.Nil(t, err)
+
+	input, output, err := ConvertLegacyMappings(mapping)
+	assert.Nil(t, err)
+
+	v, _ := json.Marshal(input)
+	fmt.Println("input:", string(v))
+
+	v2, _ := json.Marshal(output)
+	fmt.Println("output:", string(v2))
+	assert.Equal(t, "=$.body.name", input["data"].([]interface{})[0])
+	assert.Equal(t, "=$.body.ddd", input["data"].([]interface{})[1])
+}
+
+func TestConvertMappingWithObjectMapping(t *testing.T) {
+	mappings := `{
+        "input": [
+         {
+          "mapTo": "input1[0].a.b",
+          "type": "expression",
+          "value": "$.body.id"
+         },
+		 {
+				"mapto":"input1[1].a",
+				"type":"array",
+				"value":"{\r\n    \"fields\": [\r\n        {\r\n            \"from\": \"tstring.concat(\\\"this street name: \\\", \\\"ddd\\\")\",\r\n            \"to\": \"$.street\",\r\n            \"type\": \"primitive\"\r\n        },\r\n        {\r\n            \"from\": \"tstring.concat(\\\"The zipcode is: \\\",$.zipcode)\",\r\n            \"to\": \"$.zipcode\",\r\n            \"type\": \"primitive\"\r\n        },\r\n        {\r\n            \"from\": \"$.state\",\r\n            \"to\": \"$.state\",\r\n            \"type\": \"primitive\"\r\n        },\r\n\t\t{\r\n    \t\t\"from\": \"$.array\",\r\n    \t\t\"to\": \"$.array\",\r\n            \"type\": \"foreach\",\r\n\t\t\t\"fields\":[\r\n\t\t\t\t{\r\n           \t\t\t \"from\": \"$.field1\",\r\n           \t\t\t \"to\": \"$.tofield1\",\r\n           \t\t\t \"type\": \"assign\"\r\n        \t\t},\r\n\t\t\t\t{\r\n            \t\t\"from\": \"$.field2\",\r\n\t\t\t\t\t\"to\": \"$.tofield2\",\r\n            \t\t\"type\": \"assign\"\r\n        \t\t},\r\n\t\t\t\t{\r\n            \t\t\"from\": \"wangzai\",\r\n\t\t\t\t\t\"to\": \"$.tofield3\",\r\n            \t\t\"type\": \"assign\"\r\n        \t\t}\r\n\t\t\t]\r\n\r\n\t\t}\r\n    ],\r\n    \"from\": \"$activity[a1].field.addresses\",\r\n    \"to\": \".field.addresses\",\r\n    \"type\": \"foreach\"\r\n}"
+         },
+         {
+          "mapTo": "data[0]",
+          "type": "expression",
+          "value": "$.body.name"
+         },
+		{
+          "mapTo": "data[1]",
+          "type": "expression",
+          "value": "$.body.name"
+         }
+        ],
+        "output": [
+         {
+          "mapTo": "code",
+          "type": "expression",
+          "value": 200
+         },
+         {
+          "mapTo": "data.return",
+          "type": "expression",
+          "value": "$.res"
+         }
+        ]
+       }`
+
+	mapping := &LegacyMappings{}
+
+	err := json.Unmarshal([]byte(mappings), mapping)
+	assert.Nil(t, err)
+
+	input, output, err := ConvertLegacyMappings(mapping)
+	assert.Nil(t, err)
+
+	v, _ := json.Marshal(input)
+	fmt.Println("input:", string(v))
+
+	v2, _ := json.Marshal(output)
+	fmt.Println("output:", string(v2))
+	assert.Equal(t, "=$.body.name", input["data"].([]interface{})[0])
+	assert.Equal(t, "=$.body.name", input["data"].([]interface{})[1])
+	assert.Equal(t, "=$.body.id", input["input1"].([]interface{})[0].(map[string]interface{})["a"].(map[string]interface{})["b"])
+
+	//output
+	assert.Equal(t, "=$.res", output["data"].(map[string]interface{})["return"])
+	assert.Equal(t, float64(200), output["code"])
+
 }
