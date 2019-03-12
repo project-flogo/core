f526e632661c79ba04711cf7ff4471d48f6fe500
Frank Martinez <fmartinez@tibco.com>
2019-03-12 12:20:17 -0400

    add temporary support for activity type

--- activity/config.go
+++ activity/config.go
@@ -6,6 +6,9 @@ type Config struct {
 	Input    map[string]interface{} `json:"input,omitempty"`
 	Output   map[string]interface{} `json:"output,omitempty"`
 	Schemas  *SchemaConfig          `json:"schemas,omitempty"`
+
+	//DEPRECATED
+	Type string `json:"type,omitempty"`
 }
 
 type SchemaConfig struct {
