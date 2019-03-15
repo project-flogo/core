0130b97ab7a3b0d259138341253621e580cc48b9
Tracy Li <tibcotracy@gmail.com>
2019-03-15 12:08:15 -0500

    Move indexer expr to expr.go file and minor changes

--- data/expression/script/gocc/ast/ref.go
+++ data/expression/script/gocc/ast/ref.go
@@ -2,11 +2,7 @@ package ast
 
 import (
 	"fmt"
-	"github.com/project-flogo/core/data"
-	"github.com/project-flogo/core/data/coerce"
 	"github.com/project-flogo/core/data/expression/script/gocc/token"
-	"github.com/project-flogo/core/data/resolve"
-	"strconv"
 )
 
 func Concat(items ...interface{}) ([]interface{}, error) {
@@ -24,8 +20,6 @@ func Concat(items ...interface{}) ([]interface{}, error) {
 			array = append(array, t)
 		case []interface{}:
 			array = append(array, t...)
-		default:
-			array = append(array, t)
 		}
 	}
 	return array, nil
@@ -34,28 +28,7 @@ func Concat(items ...interface{}) ([]interface{}, error) {
 func Indexer(indexer interface{}) (interface{}, error) {
 	switch t := indexer.(type) {
 	case Expr:
-		return &arrayIndexer{t}, nil
+		return &arrayIndexerExpr{t}, nil
 	}
 	return nil, fmt.Errorf("invalid array indexer")
-}
-
-type arrayIndexer struct {
-	expr Expr
-}
-
-func (e *arrayIndexer) ToRef(resolver resolve.CompositeResolver, root bool, scope data.Scope) (string, error) {
-	if err := e.expr.Init(resolver, root); err != nil {
-		return "", nil
-	}
-
-	v, err := e.expr.Eval(scope)
-	if err != nil {
-		return "", fmt.Errorf("eval array index expression error: %s", err.Error())
-	}
-	index, err := coerce.ToInt(v)
-	if err != nil {
-		return "", fmt.Errorf("array index [%s] must be int", v)
-	}
-
-	return "[" + strconv.Itoa(index) + "]", nil
 }
