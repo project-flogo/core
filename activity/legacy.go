69af742423a52a6e3cf13d459766c3480daa6f11
Frank Martinez <fmartinez@tibco.com>
2019-04-24 17:03:13 -0400

    add handler name; add legacy context

--- activity/legacy.go
+++ activity/legacy.go
@@ -53,6 +53,13 @@ func LegacyRegister(ref string, activity Activity) error {
 }
 
 type LegacyCtx interface {
+<<<<<<< HEAD
+=======
+
+	// GetOutput gets the value of the specified output attribute
+	GetOutput(name string) interface{}
+}
+>>>>>>> add handler name; add legacy context
 
 	// GetOutput gets the value of the specified output attribute
 	GetOutput(name string) interface{}
