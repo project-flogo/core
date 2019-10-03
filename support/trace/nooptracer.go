e3b0a18b9271b657c7bf7399bcb60e05beb35d71
Pramod Roy <pcheruku@tibco.com>
2019-10-03 10:01:10 -0700

    missed changes

--- support/trace/nooptracer.go
+++ support/trace/nooptracer.go
@@ -3,7 +3,7 @@ package trace
 type nooptracer struct{}
 
 func (nt *nooptracer) Name() string     { return "noop-tracer" }
-func (nt *nooptracer) configure() error { return nil }
+func (nt *nooptracer) Configure() error { return nil }
 func (nt *nooptracer) Inject(flowID string, taskInstID string, format CarrierFormat, carrier interface{}) error { return nil }
 func (nt *nooptracer) Extract(format CarrierFormat, data interface{}) (interface{}, error) { return nil, nil }
 func (nt *nooptracer) SetTag(spanKey string, TagKey string, TagValue interface{}) bool { return false }
--- support/trace/registry.go
+++ support/trace/registry.go
@@ -2,10 +2,10 @@ package trace
 
 import "github.com/project-flogo/core/support/log"
 
-var tracer FlogoTracer
+var tracer Tracer
 
 // RegisterTracer registers the configured tracer
-func registerTracer(t FlogoTracer) {
+func registerTracer(t Tracer) {
 	if !isTracerRegistered() {
 		log.RootLogger().Infof("Registering tracer: %s", t.Name())
 		tracer = t
@@ -18,7 +18,7 @@ func isTracerRegistered() bool {
 
 // GetTracer returns the instance of the registered tracer.
 // If no tracer is registered, a noop tracer is returned
-func GetTracer() FlogoTracer {
+func GetTracer() Tracer {
 	if tracer == nil {
 		log.RootLogger().Warn("No tracing configuration found. registering noop-tracer")
 		tracer = &nooptracer{}
