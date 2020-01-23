796010c2d2a3026e5a3049acbb310a570bdfcfc5
Frank Martinez <fm@fm.com>
2020-01-23 14:55:39 -0500

    update go.mod; fix test support

--- support/test/action.go
+++ support/test/action.go
@@ -2,21 +2,32 @@ package test
 
 import (
 	"github.com/project-flogo/core/app/resource"
+	"github.com/project-flogo/core/support/service"
 )
 
 func NewActionInitCtx() *ActionInitCtx {
 	resources := make(map[string]*resource.Resource)
-	manager := resource.NewManager(resources)
-	return &ActionInitCtx{manager: manager, resources: resources}
+	resManager := resource.NewManager(resources)
+	svcManager := service.NewServiceManager()
+	return &ActionInitCtx{resManager: resManager, svcManager:svcManager, resources: resources}
 }
 
 type ActionInitCtx struct {
-	resources map[string]*resource.Resource
-	manager   *resource.Manager
+	resources  map[string]*resource.Resource
+	resManager *resource.Manager
+	svcManager *service.Manager
+}
+
+func (ctx *ActionInitCtx) ServiceManager() *service.Manager {
+	return ctx.svcManager
+}
+
+func (ctx *ActionInitCtx) RuntimeSettings() map[string]interface{} {
+	return make(map[string]interface{})
 }
 
 func (ctx *ActionInitCtx) ResourceManager() *resource.Manager {
-	return ctx.manager
+	return ctx.resManager
 }
 
 func (ctx *ActionInitCtx) AddResource(resourceType string, config *resource.Config) error {
