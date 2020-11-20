e590c9ed04e4517f299a915990567ed8f18a4731
Vijay Nalawade <vijaynalawade@users.noreply.github.com>
2020-11-19 17:41:23 -0800

    Flow control fixes (#236)
    * Initial implementation of flow controller
    
    * Added FlowControlAware interface for triggers. Triggers implementing that inteface can be paused/resumed by the engine.
    
    * changes based on reviews
    
    * Addressing missed fixes

--- app/controller.go
+++ app/controller.go
@@ -17,24 +17,26 @@ func (c controllerData) startController() {
 	for {
 		select {
 		case v := <-c.notify:
-			if len(c.triggers) > 0 {
-				if v && !c.flowControlled {
-					_ = c.pauseTriggers()
-				} else if !v && c.flowControlled {
-					_ = c.resumeTriggers()
-				}
+			if v == c.flowControlled {
+				// No state change
+				continue
+			}
+			c.flowControlled = v
+			if v {
+				_ = c.pauseTriggers()
+			} else if !v {
+				_ = c.resumeTriggers()
 			}
 		}
 	}
 }
 
-func GetFlowController() <-chan bool {
+func GetFlowController() chan<- bool {
 	return controller.notify
 }
 
 func (app *App) enableFlowController() {
 	controller = controllerData{}
-	controller.notify = make(chan bool)
 	controller.triggers = make(map[string]trigger.FlowControlAware)
 	for id, trgW := range app.triggers {
 		if t, ok := trgW.trg.(trigger.FlowControlAware); ok {
@@ -42,6 +44,8 @@ func (app *App) enableFlowController() {
 		}
 	}
 	if len(controller.triggers) > 0 {
+		// Initialize channel
+		controller.notify = make(chan bool)
 		go controller.startController()
 	}
 }
@@ -61,7 +65,6 @@ func (c controllerData) resumeTriggers() error {
 		log.RootLogger().Infof("Trigger [%s] is resumed.", id)
 	}
 	log.RootLogger().Info("Triggers Resumed")
-	c.flowControlled = false
 	return nil
 }
 
@@ -80,6 +83,5 @@ func (c controllerData) pauseTriggers() error {
 		log.RootLogger().Infof("Trigger [%s] is paused.", id)
 	}
 	log.RootLogger().Info("Triggers Paused")
-	c.flowControlled = true
 	return nil
 }
