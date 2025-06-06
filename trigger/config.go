5b008b8c62cd4e8173cb1fb1516aeb972318e988
Vijay Nalawade <vnalawad@tibco.com>
2025-06-06 11:54:50 +0530

    Added missing fields

--- trigger/config.go
+++ trigger/config.go
@@ -14,6 +14,7 @@ import (
 type Config struct {
 	Id       string                 `json:"id"`
 	Ref      string                 `json:"ref"`
+	Name     string                 `json:"name,omitempty"`
 	Settings map[string]interface{} `json:"settings"`
 	Handlers []*HandlerConfig       `json:"handlers"`
 
@@ -66,6 +67,7 @@ type HandlerConfig struct {
 	Name     string                 `json:"name,omitempty"`
 	Settings map[string]interface{} `json:"settings"`
 	Actions  []*ActionConfig        `json:"actions"`
+	Action   *ActionConfig          `json:"action"`
 	Schemas  *SchemaConfig          `json:"schemas,omitempty"`
 }
 
